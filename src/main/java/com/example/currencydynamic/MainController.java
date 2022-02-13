0ed3f24bfaf315311cfbffbbbe92e45e6e9d5e92
AntonPetrenko5454 <toha20055454@mail.ru>
2022-02-13 15:30:33 +0300

    13.02.2022

--- src/main/java/com/example/currencydynamic/MainController.java
+++ src/main/java/com/example/currencydynamic/MainController.java
@@ -1,8 +1,27 @@
 package com.example.currencydynamic;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 
+import java.util.Random;
+
 public class MainController {
+    @FXML
+    private LineChart lineChart;
+
+    @FXML
+    void buttonClick(ActionEvent event)
+    {
+        Random random=new Random();
+        XYChart.Series series1=new XYChart.Series();
+        for (int i=0;i<10;i++)
+        {
+            series1.getData().add(new XYChart.Data(i,random.nextInt(100)));
+        }
+        lineChart.getData().add(series1);
+    }
 
 }
